bff30324ddad80e26fe3b2758c80694e7409ce06
KingGilgamesh <1310906396@qq.com>
2018-06-14 00:07:11 +0800

    upload units

--- unitstrans.cpp
+++ unitstrans.cpp
@@ -1,47 +1,90 @@
 #include<iostream>
 #include<map>
 #include<string>
+#include<cmath> 
 using namespace std;
 typedef map<string,double> Mapsi;
 class units{
-public://å•ä½æˆå‘˜  åœ¨å•ä½ç»„åˆä¸­æ·»åŠ æ–°å•ä½çš„æ–¹æ³• 
+public://µ¥Î»³ÉÔ±insert  ÔÚµ¥Î»×éºÏÖĞÌí¼ÓĞÂµ¥Î»µÄ·½·¨ 
 	void Mapsi_insert(Mapsi *mapsi,string ch,double n){
 		mapsi->insert(Mapsi::value_type(ch,n));
 	}
-	virtual bool Find(string,string)=0;
+	virtual bool Find(string,string)=0;//ÓÉÓÚfindº¯ÊıÔÚÁ½¸öÀàÀï¶¼±»ĞèÒª£¬ËùÒÔ¶¨ÒåÒ»¸ö´¿Ğéº¯Êı×÷Îª¸¸ÀàµÄ·½·¨£» 
 private: 
 };
 class Basic_units:public units{
-
 public:
 	Basic_units():index(0){
-		//é•¿åº¦å•ä½é¢„ç½®
-		//å¸¸è§„åº”è¯¥æ˜¯length.insert(Mapsi::value_type("m",1))ï¼Œä½†æˆ‘æŠŠinsertå‡½æ•°æŠ½è±¡è¿›äº†çˆ¶ç±» 
+		//³¤¶Èµ¥Î»Ô¤ÖÃ
+		//³£¹æÓ¦¸ÃÊÇlength.insert(Mapsi::value_type("m",1))£¬µ«ÎÒ°Ñinsertº¯Êı³éÏó½øÁË¸¸Àà 
 		u[0]="length";
 		Mapsi_insert(&total[u[0]],"m",1);
-		Mapsi_insert(&total[u[0]],"km",1000);
-		Mapsi_insert(&total[u[0]],"dm",0.1);
-		Mapsi_insert(&total[u[0]],"cm",0.01);
-		Mapsi_insert(&total[u[0]],"mm",0.001);
-		Mapsi_insert(&total[u[0]],"um",0.000001);
-		Mapsi_insert(&total[u[0]],"nm",0.000000001);
-		
-		
+		Mapsi_insert(&total[u[0]],"km",0.001);
+		Mapsi_insert(&total[u[0]],"dm",10);
+		Mapsi_insert(&total[u[0]],"cm",100);
+		Mapsi_insert(&total[u[0]],"mm",1000);
+		Mapsi_insert(&total[u[0]],"um",1000000);
+		Mapsi_insert(&total[u[0]],"nm",1000000000);
+		Mapsi_insert(&total[u[0]],"nmi",0.00053996);//º£Àï 
+		Mapsi_insert(&total[u[0]],"mi",0.00062137);//Ó¢Àï
+		Mapsi_insert(&total[u[0]],"in",39.37007874);//Ó¢´ç 
+		Mapsi_insert(&total[u[0]],"yd",1.0936133);//Âë 
+		Mapsi_insert(&total[u[0]],"ft",3.2808399);//Ó¢³ß 
+		Mapsi_insert(&total[u[0]],"fur",0.00497097);//¸¥Â¡ 
+		Mapsi_insert(&total[u[0]],"Àï",0.002);
+		Mapsi_insert(&total[u[0]],"³ß",3.00030003);
+		Mapsi_insert(&total[u[0]],"·Ö",300.030003);
+		Mapsi_insert(&total[u[0]],"ÕÉ",0.30003);
+		Mapsi_insert(&total[u[0]],"´ç",30.0030003);
+		Mapsi_insert(&total[u[0]],"Àå",3000.30003);
 		
 		u[1]="weight";
+		Mapsi_insert(&total[u[1]],"g",1);
+		Mapsi_insert(&total[u[1]],"kg",0.001);
+		Mapsi_insert(&total[u[1]],"t",0.000001);
+		Mapsi_insert(&total[u[1]],"mg",1000);
+		Mapsi_insert(&total[u[1]],"dwt",0.73487421);//Ó¢Ç® 
+		Mapsi_insert(&total[u[1]],"oz",0.03527396);// °»Ë¾
+		Mapsi_insert(&total[u[1]],"lb",0.00220462);//°õ 
+		Mapsi_insert(&total[u[1]],"dr",0.56438339);//´òÀ¼ 
+		Mapsi_insert(&total[u[1]],"Á½",0.02);
+		Mapsi_insert(&total[u[1]],"Ç®",0.2);
+		Mapsi_insert(&total[u[1]],"ÊĞ½ï",0.002);
+		
 		u[2]="time";
+		Mapsi_insert(&total[u[2]],"week",1);
+		Mapsi_insert(&total[u[2]],"day",7);
+		Mapsi_insert(&total[u[2]],"h",168);
+		Mapsi_insert(&total[u[2]],"min",10080);
+		Mapsi_insert(&total[u[2]],"s",604800);
+		Mapsi_insert(&total[u[2]],"ms",604800000);
+		Mapsi_insert(&total[u[2]],"us",604800000000);//¦ÌÓÃu´úÌæ 
+		Mapsi_insert(&total[u[2]],"ns",604800000000000);
+		Mapsi_insert(&total[u[2]],"month",0.23333333333333);
+		Mapsi_insert(&total[u[2]],"year",0.019178082191781);
+		
 		u[3]="ele";
-		
-		
-		
-		
-		
+		Mapsi_insert(&total[u[3]],"A",1);
+		Mapsi_insert(&total[u[3]],"kA",pow(10,-3));
+		Mapsi_insert(&total[u[3]],"MA",pow(10,-6));
+		Mapsi_insert(&total[u[3]],"GA",pow(10,-9));
+		Mapsi_insert(&total[u[3]],"mA",pow(10,3));
+		Mapsi_insert(&total[u[3]],"uA",pow(10,6));
+		Mapsi_insert(&total[u[3]],"nA",pow(10,9));
+		Mapsi_insert(&total[u[3]],"C/s",10);
 		
 	}
 	virtual bool Find(string st1,string st2){
-		return true;
+		for(int i=0;i<4;i++){
+			Mapsi::iterator it1=total[u[i]].find(st1),it2=total[u[i]].find(st2);
+			if(it1!=total[u[i]].end()&&it2!=total[u[i]].end()){
+				index=i;
+				return true; 
+			} 
+		}
+		return false;
 	}
-	double result(int x,string st1,string st2){
+	double result(double x,string st1,string st2){
 		return x*total[u[index]][st2]/total[u[index]][st1];
 	}
 private:
@@ -50,13 +93,13 @@ private:
 	string u[4];
 };
 int main(){
-	int n;
+	double n;
 	string st1,st2;
-	cout<<"Please put in a numberï¼š";
+	cout<<"Please put in a number£º";
 	cin>>n;
-	cout<<"Please put in it's unitï¼š";
+	cout<<"Please put in it's unit£º";
 	cin>>st1; 
-	cout<<"Please put in it's next unitï¼š" ;
+	cout<<"Please put in it's next unit£º" ;
 	cin>>st2;
 	Basic_units Bu;
 	if(Bu.Find(st1,st2)){
